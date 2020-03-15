e653dc42a8858f30d46e57ff603ba4afd270f024
MrSicXit <premal.kara@hotmail.co.uk>
2020-03-15 22:59:22 +0000

    Added PDJ character controller code and mouse lock code

---
+++ Survival Horror Prototype 1/Assets/Scripts/AdminTools/MouseLock.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLock : MonoBehaviour
+{
+    public KeyCode ToggleMouseLock;
+    public bool IsCursorLocked;
+    private bool CurrentCursorLockMode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentCursorLockMode = IsCursorLocked;
+        SetMouseLockState();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(ToggleMouseLock))
+        {
+            CurrentCursorLockMode = !CurrentCursorLockMode;
+            SetMouseLockState();
+        }
+    }
+
+    void SetMouseLockState()
+    {
+        if (CurrentCursorLockMode)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
