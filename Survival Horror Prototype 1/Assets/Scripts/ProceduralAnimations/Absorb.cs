3eccbc9835e1d04754d990ba931127bd086ff5fd
MrSicXit <premal.kara@hotmail.co.uk>
2020-03-17 23:20:37 +0000

    Working on the IK system, added a humanoid rig to test it with

---
+++ Survival Horror Prototype 1/Assets/Scripts/ProceduralAnimations/Absorb.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Absorb : MonoBehaviour
+{
+    public List<GameObject> affected = new List<GameObject>();
+
+    public List<Transform> rig = new List<Transform>();
+
+    void Start()
+    {
+        TraverseHierarchy();
+    }
+
+    void Pull(Collider other)
+    {
+        if (other.GetComponent<Rigidbody>() != null && other.tag != "Projectile")
+        {
+            other.GetComponent<Rigidbody>().useGravity = false;
+            affected.Add(other.gameObject);
+
+        }
+    }
+
+    void ManipulationPoints()
+    {
+        
+
+
+    }
+
+    void TraverseHierarchy()
+    {
+        List<Transform> childs = new List<Transform>();
+
+        Utilities.GetAllChildren(transform.parent, ref childs);
+
+        rig = childs;
+        foreach (Transform t in childs)
+        {
+
+            Debug.Log(t.name);
+
+        }
+    }
+
+    //void OnTriggerExit(Collider other)
+    //{
+    //    if (affected.IndexOf(other.gameObject) != -1)
+    //    {
+    //        other.GetComponent<Rigidbody>().useGravity = true;
+    //        affected.Remove(other.gameObject);
+    //    }
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+static class Utilities
+{
+
+    public static void GetAllChildren(Transform parent, ref List<Transform> transforms)
+    {
+        foreach (Transform t in parent)
+        {
+            transforms.Add(t);
+
+            GetAllChildren(t, ref transforms);
+        }
+    }
+}
---
+++ Survival Horror Prototype 1/Assets/Scripts/ProceduralAnimations/Orbit.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+
+    public List<GameObject> affected = new List<GameObject>();
+    [SerializeField] float gravityMultiplier = 2;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Rigidbody>() != null && other.tag != "Projectile")
+        {
+            other.GetComponent<Rigidbody>().useGravity = false;
+            affected.Add(other.gameObject);
+            
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (affected.IndexOf(other.gameObject) != -1)
+        {
+            other.GetComponent<Rigidbody>().useGravity = true;
+            affected.Remove(other.gameObject);
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        foreach (GameObject cur in affected)
+        {
+            float distance = Vector3.Distance(transform.position, cur.transform.position);
+            cur.GetComponent<Rigidbody>().AddForce(Vector3.Normalize(transform.position - cur.transform.position) * (1 / distance * distance) * gravityMultiplier);
+            //this is basically newton's gravity law with the two masses each being one
+        }
+
+    }
+}
